4e294961eaaaa63f50672545eb714ab230268051
Sehriban Tas <40441222+aurora-seri@users.noreply.github.com>
2019-11-29 19:52:13 +0300

    qqqqq

--- fazilet_blog/pages/admin.py
+++ fazilet_blog/pages/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from pages.models import Page
+from pages.models import Post
 # Register your models here.
+admin.site.register(Page)
+admin.site.register(Post)
--- fazilet_blog/pages/models.py
+++ fazilet_blog/pages/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.utils import timezone
+class Page(models.Model):
+	title = models.CharField(max_length=50)
+	slug=models.CharField(max_length=50)
+	content=models.TextField()
+	is_active =models.BooleanField(default="False")
+	icon =models.CharField(max_length = 10,default = " ")
+	def __str__(self):
+		return self.title
 
-# Create your models here.
+class Post(models.Model):
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(
+            default=timezone.now)
+    published_date = models.DateTimeField(
+            blank=True, null=True)
+    upload = models.ImageField(upload_to='uploads/',blank=True)
+
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
+
+
+
