e875ce1fa9350eefa52529704dbe56c81baf22d3
Sehriban Tas <40441222+aurora-seri@users.noreply.github.com>
2019-11-29 12:40:04 +0300

    qqqqq

--- fazilet_blog/fazilet_blog/urls.py
+++ fazilet_blog/fazilet_blog/urls.py
@@ -1,9 +1,15 @@
 
 from django.contrib import admin
 from django.urls import path
-from pages.views import index_func
+from pages.views import index_func,about_func,blog_func,iletisim_func
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("",index_func)
+    path("", index_func),
+
+    path("home/index",index_func),
+    path("page/about",about_func),
+    path("page/blog",blog_func),
+    path("page/iletisim",iletisim_func)
+
 ]
--- fazilet_blog/pages/views.py
+++ fazilet_blog/pages/views.py
@@ -2,3 +2,12 @@ from django.shortcuts import render
 
 def index_func(request):
     return render(request,"home/index.html")
+
+def about_func(request):
+    return render(request,"page/about.html")
+
+def blog_func(request):
+    return render(request,"page/blog.html")
+
+def iletisim_func(request):
+    return render(request,"page/iletisim.html")
