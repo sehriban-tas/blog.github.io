33af4e03f7c82639c66e56ffd8e82e2af810ad6b
Sehriban Tas <40441222+aurora-seri@users.noreply.github.com>
2019-12-02 17:09:23 +0300

    ggg

--- fazilet_blog/pages/views.py
+++ fazilet_blog/pages/views.py
@@ -1,4 +1,4 @@
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
 from django.shortcuts import render
 from pages.models import Post
 def index_func(request):
@@ -9,9 +9,9 @@ def about_func(request):
     return render(request,"page/about.html")
 
 def blog_func(request):
-    posts = Post.objects.all().order_by('published_date')
-    paginator = Paginator(posts,2)
-    page = request.GET.get('page')
+    posts_list = Post.objects.all().order_by('published_date')
+    paginator = Paginator(posts_list,2)
+    page = request.GET.get('sayfa')
     posts = paginator.get_page(page)
     return render(request,"page/blog.html",{'posts':posts})
 
